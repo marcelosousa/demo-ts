2946394df630e4441377c9160b46ba24012884bf
Tiago Ferreira <ferreiratiago@outlook.com>
2020-02-28 16:48:42 +0000

    feat: Add language to repo details

--- src/components/Repositories/Repositories.tsx
+++ src/components/Repositories/Repositories.tsx
@@ -1,21 +1,26 @@
 import React from 'react';
+import { Owner } from '../../models/owner';
 import { Repositoyr } from '../../models/repository';
 import RepositoryDetails from '../RepositoryDetails';
 import Title from '../Title';
 
 interface Repositories {
     list: Repositoyr[];
+    owner?: Owner;
 }
 
 export default React.memo(function Repositories({ list }: Repositories) {
     return (
         <>
-            <Title>Repositories</Title>
+            <Title>List of Repositories</Title>
             <div>
                 {
                     list.map(repository => {
                         return (
-                            <RepositoryDetails content={repository} />
+                            <>
+                                <h2>{repository.name}</h2>
+                                <RepositoryDetails repo={repository} />
+                            </>
                         )
                     })
                 }
