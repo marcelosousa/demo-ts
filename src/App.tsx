648a1cbaeb4fd83abf634359282fea06d3850838
Xavier-explore-dev <xavier@explore.dev>
2020-04-13 16:16:18 +0100

    Added for loops tests

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,31 @@ import RepositoryHighlighted from './components/RepositoryHighlighted';
 import RepositoryMostForks from './components/RepositoryMostForks';
 import RepositoryMostStars from './components/RepositoryMostStars';
 import Title from './components/Title/Title';
+import * as repos from './models/repository';
+
+function testForOfLoop() {
+  for (const value of repos.MY_LIST) {
+    repos.myConsumer(value);
+  }
+}
+
+function testForInLoop() {
+  for (const value in repos.MY_LIST) {
+    repos.myConsumer(value);
+  }
+}
+
+function testForEachLoop() {
+  for (let value = repos.myInit(); repos.myCondition(value); value = repos.myIncrement(value)) {
+    repos.myConsumerInt(value);
+  }
+}
 
 function App() {
+  testForOfLoop();
+  testForInLoop();
+  testForEachLoop();
+
   return (
     <div className="App">
       <GitHubController>
--- src/models/repository.ts
+++ src/models/repository.ts
@@ -4,3 +4,31 @@ export interface Repository {
     full_name: string;
     url: string;
 }
+
+export const MY_LIST = [
+    "A",
+    "B",
+    "C"
+];
+
+export var MY_INT = 0;
+
+export function myInit(): number {
+    return 0;
+}
+
+export function myIncrement(value: number): number {
+    return value + 1;
+}
+
+export function myCondition(value: number): boolean {
+    return value < 100;
+}
+
+export function myConsumer(value: string) {
+    console.log(value);
+}
+
+export function myConsumerInt(value: number) {
+    console.log(value);
+}
