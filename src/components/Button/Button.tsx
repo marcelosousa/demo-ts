a67dec9910b3e6539d3152f64eec2a2105d7b87f
Xavier-explore-dev <xavier@explore.dev>
2020-04-13 10:00:27 +0100

    Async call and spread element

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import './Button.css';
+import * as cst from '../../models/consts';
 
 interface Button {
     children: string;
     onClick?: () => void;
+}
+
+async function callCallAsync() {
+    const list = [
+        "A",
+        ...cst.MY_ARRAY
+    ];
+
+    await cst.callAsync.call(() => list);
 }
 
 export default React.memo(function Button({ children, onClick }: Button) {
---
+++ src/models/consts.ts
@@ -0,0 +1,9 @@
+export async function callAsync() {
+    console.log("ASYNC!");
+}
+
+export var MY_ARRAY: string[] = [
+    "a",
+    "b",
+    "c"
+]
