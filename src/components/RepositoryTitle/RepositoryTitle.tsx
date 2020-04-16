849f7e9de284347d055612aca97d6df9f6ff7ee6
Xavier-explore-dev <xavier@explore.dev>
2020-04-16 14:18:29 +0100

    Two usages in same line

--- src/components/RepositoryTitle/RepositoryTitle.tsx
+++ src/components/RepositoryTitle/RepositoryTitle.tsx
@@ -5,7 +5,18 @@ interface RepositoryTitle {
     repo: Repository;
 }
 
+class RepositoryTitleImpl implements RepositoryTitle {
+    repo: Repository = {
+        id: 1,
+        name: "name",
+        full_name: "full name",
+        url: "url",
+    };
+}
+
 export default React.memo(function RepositoryTitle({ repo }: RepositoryTitle) {
+    const local: RepositoryTitleImpl = new RepositoryTitleImpl();
+
     return (
         <>
             <a href={repo.url}><h3>{repo.name}</h3></a>
