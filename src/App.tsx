ab472275b64169d9a3bf20aa7a7704140ea7400e
Xavier-explore-dev <xavier@explore.dev>
2020-04-20 17:17:14 +0100

    Added double import

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,26 @@ import RepositoryHighlighted from './components/RepositoryHighlighted';
 import RepositoryMostForks from './components/RepositoryMostForks';
 import RepositoryMostStars from './components/RepositoryMostStars';
 import Title from './components/Title/Title';
+import { Repository } from './models/repository';
+import * as repos from './models/repository';
 
 function App() {
+  var local: Repository = {
+    id: 1,
+    name: "name",
+    full_name: "full_name",
+    url: "url"
+  };
+  var local2: repos.Repository = {
+    id: 1,
+    name: "name",
+    full_name: "full_name",
+    url: "url"
+  };
+
+  console.log(local);
+  console.log(local2);
+
   return (
     <div className="App">
       <GitHubController>
