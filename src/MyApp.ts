6b8c172f3696cdfed69907f17aa6cd956455d255
Xavier-explore-dev <xavier@explore.dev>
2020-04-26 13:57:54 +0100

    Added more usages

--- src/MyApp.ts
+++ src/MyApp.ts
@@ -1,5 +1,5 @@
 import * as repos3 from './models3';
-import { repos4 } from './models4';
+import { repos4, V4 } from './models4';
 
 function myApp() {
     var v1: repos3.repos.Repository = {
@@ -8,13 +8,31 @@ function myApp() {
         full_name: "full name",
         url: "url"
     };
-    var v2: repos4.repos2.Repository = {
+    var v2: repos3.repos2.Repository = {
         id: 2,
         name: "name",
         full_name: "full name",
         url: "url"
+    };
+    var v3: repos4.repos.Repository = {
+        id: 3,
+        name: "name",
+        full_name: "full name",
+        url: "url"
+    }
+    var v4: repos4.repos2.Repository = {
+        id: 4,
+        name: "name",
+        full_name: "full name",
+        url: "url"
+    }
+    var vv4: V4 = {
+        id: "4",
     }
 
     console.log(v1);
     console.log(v2);
+    console.log(v3);
+    console.log(v4);
+    console.log(vv4);
 }
--- src/models4/index.ts
+++ src/models4/index.ts
@@ -1,1 +1,5 @@
 export * as repos4 from '../models3';
+
+export interface V4 {
+    id: string
+}
